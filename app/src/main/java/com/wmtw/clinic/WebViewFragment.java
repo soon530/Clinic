97f53eafdf50de89df2d6e015d83d0a77c36e199
vic <soon530@gmail.com>
2015-12-28 13:09:02 +0800

    Add WebView feature.

--- app/src/main/java/com/wmtw/clinic/WebViewFragment.java
+++ app/src/main/java/com/wmtw/clinic/WebViewFragment.java
@@ -6,6 +6,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.TextView;
 
 
@@ -37,20 +40,39 @@ public class WebViewFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootview = inflater.inflate(R.layout.fragment_view_web, container, false);
-        TextView content = (TextView) rootview.findViewById(R.id.content);
+
+        WebView webView = (WebView) rootview.findViewById(R.id.webView);
+
+        webView.getSettings().setSupportZoom(true);
+        webView.getSettings().setBuiltInZoomControls(true);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setUseWideViewPort(true);
+        webView.getSettings().setLoadWithOverviewMode(true);
+        webView.getSettings().setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
+        webView.setWebViewClient(new WebViewClient());
+        //webView.getSettings().setGeolocationEnabled(true);
+//        webView.setWebViewClient(new WebViewClient() {
+//            @Override
+//            public void onPageFinished(WebView view, String url){
+//                view.loadUrl(url);
+//            }
+//        });
+
+        webView.loadUrl("http://27.105.132.195:3532/ai-beauty/index.php/article/index/1");
+        //TextView content = (TextView) rootview.findViewById(R.id.content);
 
         switch (mItemId) {
             case R.id.nav_results:
-                content.setText("My results");
+                //content.setText("My results");
                 break;
             case R.id.nav_about:
-                content.setText("About");
+                //content.setText("About");
                 break;
             case R.id.nav_faq:
-                content.setText("FAQ");
+                //content.setText("FAQ");
                 break;
             case R.id.nav_contact:
-                content.setText("Contact");
+                //content.setText("Contact");
                 break;
         }
 
